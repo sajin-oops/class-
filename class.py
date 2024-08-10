6516abaadbbd8e488978e30f876116054aef214a
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-10 20:08:23 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,17 @@
+#Creating a class
+class person1:
+  def __init__(self, name,age):
+    self.name = name
+    self.age = age
+  
+#Creating a object
+obj = person1("sajin", 21)
+
+print(obj.name)
+print(obj.age)
+
+#O/P
+'''
+sajin
+21
+'''
